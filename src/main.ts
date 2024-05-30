2caec9700ddce06161b885bc91f7780e208ead6f
sreejithout <sreejithrknss@outlook.com>
2024-05-30 21:49:51 +0530

    Default app from official angular.dev

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
+/*
+ *  Protractor support is deprecated in Angular.
+ *  Protractor is used in this example for compatibility with Angular documentation tools.
+ */
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
+  console.error(err),
+);
